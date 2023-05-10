e5e25b7ea9608ce494a929097c2d1b8240e3540d
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2023-05-10 03:50:32 +0000

    chore: Flask導入

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import os
 
+from flask import Flask
 from dotenv import load_dotenv
 
 # 開発用.env読み込み、本番環境ではエラーを出さずに無視される
 load_dotenv(".env")
+
+# Flask設定
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello, World!"
